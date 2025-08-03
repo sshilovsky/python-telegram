31c832fe872fce79f72a0f0a19e1cd261b210ddc
Sergei Shilovsky <sshilovsky@gmail.com>
2025-08-03 13:19:26 +0300

    add_update_handler as decorator

--- telegram/client.py
+++ telegram/client.py
@@ -580,7 +580,10 @@ class Telegram:
     def add_message_handler(self, func: Callable) -> None:
         self.add_update_handler(MESSAGE_HANDLER_TYPE, func)
 
-    def add_update_handler(self, handler_type: str, func: Callable) -> None:
+    def add_update_handler(self, handler_type: str, func: Optional[Callable]) -> None:
+        if func is None:
+            # decorator-style
+            return lambda func: self.add_update_handler(handler_type, func)
         if func not in self._update_handlers[handler_type]:
             self._update_handlers[handler_type].append(func)
 
